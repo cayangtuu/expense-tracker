246309cd3433bc60ce550676da4f54072dd77a79
cayang <cayang@simenvi.com.tw>
2023-02-17 09:16:06 +0800

    refactor: seeders password add bcrypt

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const db = require('../../config/mongoose')
 const Category = require('../Category')
 const Record = require('../Record')
@@ -6,20 +7,29 @@ const recordList = require('./record.json').results
 const userList = require('./user.json').results
 
 db.once('open', () => {
-  return User.create(userList[0])
-    .then(user => {
-      return Promise.all(
-        Array.from(recordList, async record => {
-          record.userId = user._id
-          const category = await Category.findOne({ name: record.category }).lean()
-          record.categoryId = category._id
-          await Record.create(record)
-        }))
-        .catch(err => console.log(err))
-    })
-    .then(() => {
-      console.log('done')
-      process.exit()
-    })
-    .catch(err => console.log(err))
+  userList.map(user => {
+    return bcrypt
+      .genSalt(12)
+      .then(salt => bcrypt.hash(user.password, salt))
+      .then(hash => User.create({
+        user: user.name,
+        email: user.email,
+        password: hash
+      }))
+      .then(user => {
+        return Promise.all(
+          Array.from(recordList, async record => {
+            record.userId = user._id
+            const category = await Category.findOne({ name: record.category }).lean()
+            record.categoryId = category._id
+            await Record.create(record)
+          }))
+          .catch(err => console.log(err))
+      })
+      .then(() => {
+        console.log('done')
+        process.exit()
+      })
+      .catch(err => console.log(err))
+  })
 })
