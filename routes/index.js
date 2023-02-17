26fe17ab6be8037db7ce0577ddfe8e83fbb8cbd7
cayang <cayang@simenvi.com.tw>
2023-02-17 08:56:58 +0800

    feat: add auth middleware

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const home = require('./modules/home')
 const records = require('./modules/records')
 const users = require('./modules/users')
 const { generalErrorHandler } = require('../middleware/error')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/records', records)
+router.use('/records', authenticator, records)
 router.use('/users', users)
-router.use('/', home)
+router.use('/', authenticator, home)
 router.use('/', generalErrorHandler)
 
 module.exports = router
